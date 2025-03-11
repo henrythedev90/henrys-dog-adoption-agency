d65cf82d4801fcef4897ebed43f1e4d6eafab597
henrythedev90 <henrythedev90@gmail.com>
2025-03-11 17:28:04 -0400

    created the auth routes login, logout, check

---
+++ henry-dog-adoption-app/pages/api/auth/check.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const cookies = req.headers.cookie;
+
+  if (!cookies) {
+    return res.status(401).json({ error: "You are not authorize" });
+  }
+  return res.status(200).json({ success: true });
+}
---
+++ henry-dog-adoption-app/pages/api/auth/login.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const url = "https://frontend-take-home-service.fetch.com";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    return res.status(405).json({ error: "Method is not allowed" });
+  }
+
+  const { name, email } = req.body;
+
+  if (!email || !name) {
+    return res
+      .status(400)
+      .json({ message: "Email and/or password are required" });
+  }
+
+  try {
+    await axios.post(
+      url + "/auth/login",
+      { name, email },
+      { withCredentials: true }
+    );
+    res
+      .status(200)
+      .json({ success: true, message: "I hope you find your dog" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to authenticate" });
+  }
+}
