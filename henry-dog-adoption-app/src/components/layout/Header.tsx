f34d2c0978f8b37835addf79b5c3aa6b916e4b54
henrythedev90 <henrythedev90@gmail.com>
2025-04-12 15:28:21 -0400

    made the navbar responsive

--- henry-dog-adoption-app/src/components/layout/Header.tsx
+++ henry-dog-adoption-app/src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppSelector, useAppDispatch } from "@/store/hooks";
 import { selectDogFavorite } from "@/store/selectors/dogsSelectors";
 import { logoutUser } from "@/store/slices/authSlice";
@@ -15,6 +15,7 @@ export default function Header() {
   const dispatch = useAppDispatch();
   const favorites = useAppSelector(selectDogFavorite);
   const { isLoggedIn } = useAppSelector((state) => state.auth);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogout = async () => {
     await dispatch(logoutUser());
@@ -25,22 +26,64 @@ export default function Header() {
 
   const isFavoritesPage = pathname === "/dogs/favorites";
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={classes.header}>
       <Container>
         <div className={classes.nav_wrapper}>
-          <h3>The Fetch Adoption App</h3>
-          <div className={classes.nav_menu}>
+          <h3>The Fetch Adoption Center</h3>
+          <div
+            className={`${classes.nav_menu} ${
+              isMenuOpen ? classes.nav_menu_open : ""
+            }`}
+          >
             {isFavoritesPage ? (
-              <Link href="/dogs">Back to Search</Link>
+              <Link href="/dogs" className={classes.nav_link}>
+                Back to Search
+              </Link>
             ) : (
-              <Link href="/dogs/favorites">
+              <Link href="/dogs/favorites" className={classes.nav_link}>
                 View Favorites ({favorites.length})
               </Link>
             )}
+
             {isLoggedIn && (
-              <Button onClickFunction={handleLogout}>Logout</Button>
+              <Button
+                onClickFunction={handleLogout}
+                variant="secondary"
+                className={classes.nav_button}
+              >
+                Logout
+              </Button>
             )}
+
+            <div className={classes.mobile_logo}>
+              <Link href="/" className={classes.logo_link}>
+                <h1 className={classes.mobile_logo_title}>
+                  <span>F</span>etch
+                </h1>
+              </Link>
+            </div>
+          </div>
+          <div className={classes.hamburger} onClick={toggleMenu}>
+            <div
+              className={`${classes.hamburger_line} ${
+                isMenuOpen ? classes.hamburger_line_open : ""
+              }`}
+            />
+            <div
+              className={`${classes.hamburger_line} ${
+                isMenuOpen ? classes.hamburger_line_open : ""
+              }`}
+            />
+            <div
+              className={`${classes.hamburger_line} ${
+                isMenuOpen ? classes.hamburger_line_open : ""
+              }`}
+            />
           </div>
         </div>
       </Container>
