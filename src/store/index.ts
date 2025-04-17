d11f40b8d29e9e8dbb2a449afe73a932f932918d
henrythedev90 <henrythedev90@gmail.com>
2025-04-17 17:24:22 -0400

    fixing error before deployment and deleting files

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import dogsReducer from "./slices/dogsSlice";
 import filtersReducer from "./slices/filtersSlice";
-import locationReducer from "./slices/locationSlice";
 import breedReducer from "./slices/breedSlice";
 
 export const store = configureStore({
@@ -11,7 +10,6 @@ export const store = configureStore({
     breeds: breedReducer,
     dogs: dogsReducer,
     filters: filtersReducer,
-    location: locationReducer,
   },
 });
 
